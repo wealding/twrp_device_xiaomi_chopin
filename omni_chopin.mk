dcd50dde71ff62c944aa00c171c29f7ad7da40da
jvlong520 <together08@yeah.net>
2021-06-30 16:59:01 +0800

    chopin: Fix inherit

--- omni_chopin.mk
+++ omni_chopin.mk
@@ -1,9 +1,8 @@
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, build/target/product/embedded.mk)
 
-# Inherit some common omni stuff
-$(call inherit-product, vendor/omni/config/common_full_phone.mk)
+# Inherit some common omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from chopin device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
