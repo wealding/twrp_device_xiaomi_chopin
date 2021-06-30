90ab73efea1ce7755f1ca70c624a3e58d0926328
together08 <together08@yeah.net>
2021-06-30 16:53:08 +0800

    chopin: Add missing omni_chopin.mk

---
+++ omni_chopin.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common omni stuff
+$(call inherit-product, vendor/omni/config/common_full_phone.mk)
+
+# Inherit from chopin device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := redmi
+PRODUCT_DEVICE := chopin
+PRODUCT_MANUFACTURER := redmi
+PRODUCT_NAME := omni_chopin
+PRODUCT_MODEL := chopin
+
+PRODUCT_GMS_CLIENTID_BASE := android-redmi
+TARGET_VENDOR := redmi
+TARGET_VENDOR_PRODUCT_NAME := chopin
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="chopin-user 11 RP1A.200720.011 21.6.23 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := redmi/chopin/chopin:11/RP1A.200720.011/21.6.23:user/release-keys
