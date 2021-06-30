bff92dc09344fd061b7ae3e8e20822f8d127d31f
jvlong520 <together08@yeah.net>
2021-06-30 16:56:35 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,10 +6,32 @@ AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \ 
     boot \
     system \
-    vendor
+    vendor 
 
 AB_OTA_POSTINSTALL_CONFIG += \ 
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
+
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    bootctrl.mt6891 \
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.mt6891 \
+    libcutils \
+    libgptutils \
+    libz
